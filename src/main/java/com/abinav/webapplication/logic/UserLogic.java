6495a8f2f293ef22d97f98cb278074a4bc151632
abinav14-java <abinav14022004@gmail.com>
2025-11-15 20:21:17 +0530

    changes by abinav

--- src/main/java/com/abinav/webapplication/logic/UserLogic.java
+++ src/main/java/com/abinav/webapplication/logic/UserLogic.java
@@ -1,18 +1,32 @@
 package com.abinav.webapplication.logic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import com.abinav.webapplication.model.Users;
 import com.abinav.webapplication.service.UserService;
 
 @Component
-public class UserLogic {
+public class UserLogic  implements UserDetailsService{
 	@Autowired
 	private UserService userService;
 
-	public Users createUser(Users user) throws Exception{
+	public Users createUser(Users user) throws Exception {
 		return userService.createUser(user);
+
+	}
+
+	public boolean existsByEmail(String email) throws Exception {
+		return userService.existsByEmail(email);
+	}
+
+	public UserDetails loadUserByUsername(String email) {
+		try {
+			return userService.loadUserByUsername(email);
+		} catch (Exception e) {
+			return null;		}
 		
 	}
 
--- src/main/java/com/abinav/webapplication/service/UserService.java
+++ src/main/java/com/abinav/webapplication/service/UserService.java
@@ -1,10 +1,15 @@
 package com.abinav.webapplication.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.abinav.webapplication.model.Users;
 
 public interface UserService {
 
-	public Users createUser(Users user) throws Exception;
+	Users createUser(Users user) throws Exception;
+
+	boolean existsByEmail(String email) throws Exception;
+
+	UserDetails loadUserByUsername(String email) throws Exception;
 
 }
-
